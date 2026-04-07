36c1298c7d08fa6f90b48702fdc42f9de19f613b
Arpit-1310 <marpit1510@gmail.com>
2026-04-07 21:48:36 +0530

    logout user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
@@ -20,5 +21,8 @@ userRouter.route("/register").post(
 );
 
 userRouter.route("/login").post(loginUser);
+
+//secured routes
+userRouter.route("/logout").post(verifyJWT, logoutUser);
 
 export { userRouter }
